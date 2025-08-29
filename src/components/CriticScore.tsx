cd9d84d222e79cf6f398721b568fbdc8b7464f81
Elizabeth Maria <elizabe2@buffalo.edu>
2025-08-29 11:02:01 -0700

    Updated padding for CriticScore component

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -8,7 +8,7 @@ interface Props {
 const CriticScore = ({ score }: Props) => {
   let color = score > 80 ? "green" : score > 60 ? "yellow" : "";
   return (
-    <Badge colorScheme={color} fontSize="14px" borderRadius="4px" padding={2}>
+    <Badge colorScheme={color} fontSize="14px" borderRadius="4px" paddingX={2}>
       {score}
     </Badge>
   );
