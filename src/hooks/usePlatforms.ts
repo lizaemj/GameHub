7e87de12e2032356bf4c2f458445e21bc347a8a5
Elizabeth Maria <elizabe2@buffalo.edu>
2025-09-04 10:23:19 -0700

    Ship platforms with app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import platforms from "../data/platforms";
 
 interface Platform{
     id: number;
@@ -6,6 +7,10 @@ interface Platform{
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+// To call directly from backend
+//const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+
+// To load from static data, for data that is static
+const usePlatforms = () => ({data: platforms, error: null})
 
 export default usePlatforms
